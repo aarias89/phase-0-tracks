a82e89ad13fb0f11b8b74ef919e9d961fc9dc2c6
aarias89 <andresar11@gmail.com>
2017-07-09 22:07:21 -0400

    Release 1 create method for Fibonacci terms and give out new array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -16,6 +16,21 @@ array [1, 3, 5, 7, 9]
 
 search_array([1,3,5,7,9],9)
 
-
+#Release 1
+ +#declare an array
+ +  #array = [0,1,7,5,6,11]-->    <--
+ +  #[-1]+[-2]=num   array<<num
+ +  #<< to add product of last 2 items in array
+ +#takes a number of fibonacci terms -- argurment will be "i" for integer
+ +#return a new array with sum included in the -1 index
+ +#break if the argument number is reached
+ +
+ +def fib(i)
+ +  array = [0,1]
+ +  until array.length == i
+ +  array<<array[-1] + array[-2]
+ +  end
+ +  p array
+ +end
 
 
