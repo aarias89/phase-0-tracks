101d9e752053d5356f2985f8677c7b0464983331
aarias89 <andresar11@gmail.com>
2017-07-17 03:59:10 -0400

    Puppy class added

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Large Puppy
+
+# CHARACTERISTICS ----------------
+# weight:heavy
+# fur_color:black
+# legs:4
+# energy:high
+
+# BEHAVIOR -----------------------
+# run
+# bark
+# eat
+# cuddle
