077861c2a8fab8e9fa3f4076456faa6655ddc77c
aarias89 <andresar11@gmail.com>
2017-08-07 00:36:25 -0400

    add release 1,2,3

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,26 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a /contact route that displays an address (you can make up address)
+get '/contact' do
+  "Somewhere NY 90380"
+end
+# write a /great_job route that can take a person's name as a query parameter and say "Good job, [person's name]!" and "Good job!" is the query param is not present
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# route that uses route parameters to add 2 nums and respond with the result
+get '/add/:num_1/:num_2' do
+  num1 = params[:num_1].to_i
+  num2 = params[:num_2].to_i
+  sum = (num1+num2).to_s
+  "The sum of #{num1} and #{num2} is equal to #{sum}!"
+end
