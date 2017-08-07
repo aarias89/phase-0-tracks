9cd67cb2bed0a1270b8b39b019149fe8044d6314
aarias89 <andresar11@gmail.com>
2017-08-07 00:13:08 -0400

    image add, cass, js, and jQuery link and test: OK

---
+++ web_dev/jquery/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+   $('#prize1').on('click', function(){
+    $('#blender').toggle();
+   });
+   $('#prize2').on('click', function(){
+    $('#car').toggle();
+   });
+   $('#prize3').on('click', function(){
+    $('#dog').toggle();
+   });
+
+
+ });
+
