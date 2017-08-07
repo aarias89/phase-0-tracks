ef515473307a40c2e3e251455f08f43fc2504d55
aarias89 <andresar11@gmail.com>
2017-08-06 20:15:55 -0400

    9.2 pair: create/link index and js  page.

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,7 @@
+var header = document.getElementById("header");header.style.fontStyle='italic';
+
+function reveal(){
+  document.getElementById("jsInteraction").style.display = 'block';
+}
+
+// var jsInteraction=document.getElementById("jsInteraction");
